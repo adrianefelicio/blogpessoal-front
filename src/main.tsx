f43cc38ad6920e5f0e8e8ca095106909415bea14
Adriane Felicio <adrianefelicios@outlook.com>
2023-05-18 21:11:23 -0300

    Router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-ReactDOM.render(
+ReactDOM.render( //método obrigatório, pois ele que vai renderizar, montar a tela completa
   <React.StrictMode>
     <App />
   </React.StrictMode>,
